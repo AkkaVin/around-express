68cad71aa09bab7f7dd12ec23256ff2cbf20cc60
Akkavin <vinarski.a@gmail.com>
2022-08-25 13:34:27 +0300

    Users C&R done

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
+// get user(s) ======================================================================
 module.exports.getUsers = (req, res) => {
   User.find({})
     .orFail(() => {
@@ -24,11 +25,38 @@ module.exports.getUser = (req, res) => {
     .catch((err) => res.status(500)
       .send({ message: `An error has occurred on the server: ${err.toString()}` }));
 };
-
+// create user ======================================================================
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
     .catch((err) => res.status(500)
+    .send({ message: `An error has occurred on the server: ${err.toString()}` }));
+};
+
+// update user ======================================================================
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate ( req.user._id, { name: name, about: about})
+  .orFail(() => {
+    const error = new Error("No user found with that id");
+    error.statusCode = 404;
+    throw error;
+  })
+  .then(user => res.send({ data: user}))
+  .catch((err) => res.status(500)
+    .send({ message: `An error has occurred on the server: ${err.toString()}` }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate ( req.user._id, { avatar: avatar})
+  .orFail(() => {
+    const error = new Error("No user found with that id");
+    error.statusCode = 404;
+    throw error;
+  })
+  .then(user => res.send({ data: user}))
+  .catch((err) => res.status(500)
     .send({ message: `An error has occurred on the server: ${err.toString()}` }));
 };
