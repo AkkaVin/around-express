ae801f489d297e42d6776da4e81d1ab925377724
Akkavin <vinarski.a@gmail.com>
2022-08-29 20:58:36 +0300

    Cards C&R done

--- controllers/cards.js
+++ controllers/cards.js
@@ -39,3 +39,34 @@ module.exports.createCard = (req, res) => {
     .catch(() => res.status(500)
     .send({ message: `An error has occurred on the server: ${err.toString()}` }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
+    { new: true },
+  )
+  .orFail(() => {
+    const error = new Error("No card found with that id");
+    error.statusCode = 404;
+    throw error;
+  })
+  .then(card => res.send({ data: card}))
+  .catch((err) => res.status(500)
+    .send({ message: `An error has occurred on the server: ${err.toString()}` }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } }, // remove _id from the array
+  { new: true },)
+  .orFail(() => {
+    const error = new Error("No card found with that id");
+    error.statusCode = 404;
+    throw error;
+  })
+  .then(card => res.send({ data: card}))
+  .catch((err) => res.status(500)
+    .send({ message: `An error has occurred on the server: ${err.toString()}` }));
+}
--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const {
   getCards,
   deleteCard,
-  createCard
+  createCard,
+  likeCard,
+  dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
@@ -10,5 +12,9 @@ router.get('/cards', getCards);
 router.delete('/cards/:cardId', deleteCard);
 
 router.post ('/cards', createCard);
+
+router.put('/cards/:cardId/likes', likeCard);
+
+router.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = router;
